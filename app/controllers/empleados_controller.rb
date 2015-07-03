f00f7e5782d8c25a72318286382e680d7834adbd
Camilo Ordonez <icoj95@gmail.com>
2015-07-02 22:06:06 -0700

    Add works and Index view is displaying list

--- app/controllers/empleados_controller.rb
+++ app/controllers/empleados_controller.rb
@@ -1,5 +1,20 @@
 class EmpleadosController < ApplicationController
+  
+  def index
+      @empleados = Empleado.all
+  end
+
   def new
+      @empleado = Empleado.new
+  end
+
+  def create
+      @empleado = Empleado.new empleado_params
+      if @empleado.save
+         redirect_to root_path
+      else
+         render :new
+      end
   end
 
   def edit
@@ -10,4 +25,11 @@ class EmpleadosController < ApplicationController
 
   def update
   end
+
+  private
+
+  def empleado_params
+      params.require(:empleado).permit(:nombres, :apellidos, :cedula)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-resources :empleados
+
+  root 'empleados#index'
+  resources :empleados
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
