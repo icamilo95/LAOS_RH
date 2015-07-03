9032712618be60f4b9083d2873cd810a1b4c4af4
Camilo Ordonez <icoj95@gmail.com>
2015-07-02 22:16:38 -0700

    Edit Empleado works

--- app/controllers/empleados_controller.rb
+++ app/controllers/empleados_controller.rb
@@ -18,18 +18,26 @@ class EmpleadosController < ApplicationController
   end
 
   def edit
+   @empleado = Empleado.find params[:id]
   end
 
   def show
   end
 
   def update
+   @empleado = Empleado.find params[:id]
+   if @empleado.update_attributes empleado_params
+      redirect_to root_path
+   else
+      render :edit
+   end
+
   end
 
   private
 
   def empleado_params
-      params.require(:empleado).permit(:nombres, :apellidos, :cedula)
+      params.require(:empleado).permit(:nombres, :apellidos, :cedula, :ciudad, :sexo, :cargo, :puesto)
   end
 
 end
